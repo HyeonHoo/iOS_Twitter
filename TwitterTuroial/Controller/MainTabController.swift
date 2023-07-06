8adb6be4e756a626d01e3f897396c6ffc13f3b88
HyeonHoo <easyhoho@naver.com>
2023-07-06 16:05:15 +0900

    Create MVVM / Connect MainTabController

--- TwitterTuroial/Controller/MainTabController.swift
+++ TwitterTuroial/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterTuroial
 //
 //  Created by HyeonHo on 2023/07/06.
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
+       
     }
+    
 
+   
 
 }
-
